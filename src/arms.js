000ad9024a520d906ee95b8642aae00c56b66fea
Jeff Meyers <jeffreym.meyers@gmail.com>
2011-12-03 14:44:05 -0500

    Fixing formatting, added settings and default values.

--- src/arms.js
+++ src/arms.js
@@ -6,22 +6,44 @@ function writeLog(log) {
 // Our global "database"
 function FauxBase() {
 	// Beer values
-	this.beer = { "calories" : 150 };
+	this.beer = {
+		"calories" : 150
+	};
 
 	// Wine values
-	this.wine = { "calories" : 120 };
+	this.wine = {
+		"calories" : 120
+	};
 
 	// Liquor values
-	this.liquor = { "calories" : 100 };
+	this.liquor = {
+		"calories" : 100
+	};
 	
 	// Drinks list
-	this.drinks = { 	"beer"   : 	this.beer,
-							"wine"   : 	this.wine,
-							"liquor" : 	this.liquor };
+	this.drinks = {
+		"beer" 	: this.beer,
+		"wine" 	: this.wine,
+		"liquor" : this.liquor
+	};
 
 	// User settings
-	//this.settings = { "name" : , "};
-
+	// All default limit values are the legal limit
+	// All settings deemed to be a safety feature are true by default
+	this.settings = {
+		"weight" 				: "", 		// Integer
+		"sex" 					: "", 		// String
+		"name" 					: "", 		// String
+		"contactlocklimit" 	: 0.8, 		// Float, BAC value
+		"calltaxionlimit" 	: true, 		// Boolean
+		"calltaxilimit" 		: 0.8, 		// Float, BAC value
+		"usegps" 				: true, 		// Boolean
+		"baccalculate" 		: true, 		// Boolean
+		"cachetaxi" 			: true, 		// Boolean
+		"taxiinterval" 		: 30, 		// Integer, minutes
+		"calltaxi" 				: false 		// Boolean
+	};
+	
 	this.getDrinks = function() {
 		return this.drinks;
 	}
@@ -40,21 +62,36 @@ function DatabaseManager() {
 
 	// Grab our actual data
 	this.base = new FauxBase();
-	
+
+	// Load a table to perform operations on
 	this.loadTable = function(tableName) {
 		writeLog("Loading table " + tableName + " ... ");
+
+		// currentDatabase remains null if an invalid name is provided
+		// Also return false on failure, true on success
 		switch(tableName.toLowerCase()) {
 			case "drinks":
 				this.currentDatabase = this.base.getDrinks();
 				break;
-			default: break;
+			case "settings":
+				this.currentDatabase = this.base.getSettings();
+				break;
+			default: return false;
 		}
+
+		return true;
 	};
 
+	// Get the current database, useful for testing or list processing
 	this.getDatabase = function() {
 		return this.currentDatabase;
 	};
 
+	// Get the value of a particular entry in the table
+	// Ex. if table "drinks" is loaded, get row "beer":
+	// 	dbManager.loadTable("drinks");
+	// 	dbManager.getValueOf("beer"):
+	// 	-> returns FauxBase.beer
 	this.getValueOf = function(valueName) {
 		writeLog("Finding value of " + valueName + " ... ");
 
@@ -82,16 +119,16 @@ function DatabaseManager() {
 
 // Add a drink to our consumption table
 function addDrink(drinkName) {
-  dbManager = new DatabaseManager();
+	dbManager = new DatabaseManager();
 
-  dbManager.loadTable("drinks");
+	dbManager.loadTable("drinks");
 
-  drinkName = drinkName.toLowerCase();
+	drinkName = drinkName.toLowerCase();
 
-  // Check if drink is in the "database"
-  if(dbManager.getValueOf(drinkName) != -1) {
-    // Get the value for the entry
-  }
+	// Check if drink is in the "database"
+	if(dbManager.getValueOf(drinkName) != -1) {
+		// Get the value for the entry
+	}
 }
 
 /*
@@ -99,34 +136,34 @@ function addDrink(drinkName) {
  * Also changes button to selected or deselected and handles disabling other elements for a clean transition
 */
 function changeFlyoutVisibility(name) {
-  // Get flyout (content) div
-  var flyout = $("#"+name+'_flyout');
+	// Get flyout (content) div
+	var flyout = $("#"+name+'_flyout');
 
-  // Get button that was clicked
-  var button = $("#"+name+'_button');
+	// Get button that was clicked
+	var button = $("#"+name+'_button');
 
-  // Check if flyout is currently visible
-  if(flyout.is(":visible")) {
-    // If it is, fade it out and de-select button
-    flyout.fadeOut();
-    button.removeClass('selected');
-  } else {
-    // Otherwise, de-select all other buttons
-    $("#mainmenu").find('li').removeClass('selected');
-    
-    // Hide all flyout menus
-    $(".flyout").hide();
-   
-    // Fade in the desired flyout menu
-    flyout.fadeIn();
+	// Check if flyout is currently visible
+	if(flyout.is(":visible")) {
+		// If it is, fade it out and de-select button
+		flyout.fadeOut();
+		button.removeClass('selected');
+	} else {
+		// Otherwise, de-select all other buttons
+		$("#mainmenu").find('li').removeClass('selected');
+			
+		// Hide all flyout menus
+		$(".flyout").hide();
 
-    // Show that the current button is selected
-    button.addClass('selected');
+		// Fade in the desired flyout menu
+		flyout.fadeIn();
 
-    // Position the arrow to be above the current button
-    var x = button.position().left + ( (button.width() / 2) - ( $(".flyoutarrow").width() / 2 ) );
-    flyout.find('div').css('left', x);
-  }
+		// Show that the current button is selected
+		button.addClass('selected');
+
+		// Position the arrow to be above the current button
+			var x = button.position().left + ( (button.width() / 2) - ( $(".flyoutarrow").width() / 2 ) );
+		flyout.find('div').css('left', x);
+	}
 }
 
 function runTests() {
@@ -143,20 +180,19 @@ function runTests() {
  * Set click bindings, initial position and visibility, etc.
 */
 function init() {
-	writeLog("blah");
 	// Run tests
 	runTests();
 
-  // Flyout click bindings
-  $("#plus_one_button").click(function() {
-    changeFlyoutVisibility('plus_one');
-  });
-  $("#settings_button").click(function() {
-    changeFlyoutVisibility('settings');
-  });
-  $("#utilities_button").click(function() {
-    changeFlyoutVisibility('utilities');
-  });
+	// Flyout click bindings
+	$("#plus_one_button").click(function() {
+		changeFlyoutVisibility('plus_one');
+	});
 
-  // Module click bindings
+	$("#settings_button").click(function() {
+		changeFlyoutVisibility('settings');
+	});
+
+	$("#utilities_button").click(function() {
+		changeFlyoutVisibility('utilities');
+	});
 }
